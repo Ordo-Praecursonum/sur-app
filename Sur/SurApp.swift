60705035231d9022d35a847dd37231b5260df3d3
elielnfinic <46634774+elielnfinic@users.noreply.github.com>
2025-10-04 09:38:35 +0300

    Initial Commit

---
+++ Sur/SurApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurApp.swift
+//  Sur
+//
+//  Created by Mathe Eliel on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SurTests/SurTests.swift
@@ -0,0 +1,17 @@
+//
+//  SurTests.swift
+//  SurTests
+//
+//  Created by Mathe Eliel on 04/10/2025.
+//
+
+import Testing
+@testable import Sur
+
+struct SurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
